cb8a722247e86d175639973d03f29fb35b36926e
김민철 <rlaalscjf739789@gmail.com>
2025-09-01 09:21:16 +0900

    첫커밋

---
+++ src/main/java/com/example/docksystem_erp/DocksystemErpApplication.java
@@ -0,0 +1,13 @@
+package com.example.docksystem_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocksystemErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocksystemErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/docksystem_erp/DocksystemErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docksystem_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocksystemErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
